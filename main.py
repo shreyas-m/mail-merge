9a117610e2eb462083c39744e8d167c29a383aca
Shreyas Mahajan <maharaj.shreyu@gmail.com>
2024-07-24 22:37:23 +0100

    final

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-#TODO: Create a letter using starting_letter.txt 
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+with open("./Input/Letters/starting_letter.txt") as letter:
+    template = letter.read()
+    with open("./Input/Names/invited_names.txt") as people:
+        for name in people.readlines():
+            body = template.replace("[name]", name.strip("\n"))
+            with open(f"./Output/ReadyToSend/Letter_for_{name}.txt", "w") as readyLetter:
+                readyLetter.write(body)
